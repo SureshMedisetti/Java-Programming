e79929dbe899ac539d25c4b8a8810bca4c36bbe9
SureshMedisetti <sureshmedisetti07@gmail.com>
2026-08-23 21:56:06 +0530

    Praticed on Inheritance

---
+++ Practice/AnimalInherit.java
@@ -0,0 +1,24 @@
+class Animal{
+    public void eat(){
+        System.out.println("Eating..");
+    }
+
+    public void sleep(){
+        System.out.println("Sleeping..");
+    }
+}
+
+class Dog extends Animal{
+    public void bark(){
+        System.out.println("Barking..");
+    }
+}
+
+public class AnimalInherit {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.sleep();
+        dog.bark();
+    }
+}
