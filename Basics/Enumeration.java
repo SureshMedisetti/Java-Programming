ea589cfa8f3ed8c53b8b88507f1fe9c804416784
SureshMedisetti <sureshmedisetti07@gmail.com>
2026-08-27 13:04:22 +0530

    Learned Enums

---
+++ Basics/Enumeration.java
@@ -0,0 +1,16 @@
+enum Day{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
+public class Enumeration {
+    public static void main(String[] args) {
+        Day today = Day.Thursday;
+        System.out.println(today);
+    }
+}
